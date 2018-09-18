af192ea8c2972e5f84556f391005a5ed964e53b8
maestrow <andrew.shmelev@gmail.com>
2018-09-18 14:01:33 +0300

    Prettier, styleguidist settings, vs code settings

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     propsParser: require('react-docgen-typescript').parse,
-    webpackConfig: require('react-scripts-ts/config/webpack.config.dev')
+    webpackConfig: require('react-scripts-ts/config/webpack.config.dev'),
+    styles: {
+    Playground: {
+      preview: {
+        fontFamily: 'sans-serif',
+        margin: 0,
+        padding: 0
+      }
+    }
+  }
 }
